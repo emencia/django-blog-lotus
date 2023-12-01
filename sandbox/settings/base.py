e66f8b7a55690f80f8543ac2a34275c8fbcd77c2
David THENON <sveetch@gmail.com>
2023-12-01 02:04:54 +0000

    [WIP] Tried to enable django-spectacular, it works but needs some adjustment, see #63

--- sandbox/settings/base.py
+++ sandbox/settings/base.py
@@ -247,6 +247,8 @@ else:
     API_AVAILABLE = True
     INSTALLED_APPS.extend([
         "rest_framework",
+        'drf_spectacular',
+        'drf_spectacular_sidecar',  # required for Django collectstatic discovery
     ])
 
     REST_FRAMEWORK = {
@@ -259,9 +261,20 @@ else:
             # "rest_framework.permissions.DjangoModelPermissions",
         ],
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-        "PAGE_SIZE": 20
+        "PAGE_SIZE": 20,
+        'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     }
 
+    SPECTACULAR_SETTINGS = {
+        'TITLE': 'Your Project API',
+        'DESCRIPTION': 'Your project description',
+        'VERSION': '1.0.0',
+        'SERVE_INCLUDE_SCHEMA': False,
+        'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
+        'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+        'REDOC_DIST': 'SIDECAR',
+        # OTHER SETTINGS
+    }
 
 """
 SPECIFIC BASE APPLICATIONS SETTINGS BELOW
